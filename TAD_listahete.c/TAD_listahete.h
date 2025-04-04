1e392f4c446962ecea7e195384fafd7b7205bb94
Isabela-Linhares-Silva <isabelalinharessilva@gmail.com>
2025-04-04 09:49:03 -0300

    Estudo lista heterogênea

---
+++ TAD_listahete.c/TAD_listahete.h
@@ -0,0 +1,48 @@
+#ifndef __TAD_LISTAHETE_H__
+    #define __TAD_LISTAHETE_H__
+
+    //chora buaaaaa buaa
+    #include<stdio.h>
+    #define RET 0
+    #define CIR 1
+    #define TRI 2
+
+    typedef struct listahet
+    {
+        int id;
+        int tipo;
+        void* info;
+        struct listahet * prox; 
+    }Listahet;
+
+    typedef struct circulo
+    {
+        float raio;
+    }Cir;
+
+    typedef struct retangulo
+    {
+        float base;
+        float altura;
+    }Ret;
+
+    typedef struct triangulo
+    {
+        float base;
+        float altura;
+    }Tri;
+
+    Listahet* cria_lista();
+    Listahet * insere_lista(Listahet* cabeca, int id, int tipo, void* info);
+    Listahet* libera_lista(Listahet* cabeca);
+    Listahet* busca_lista(Listahet* cabeca, int id);
+    void* cria_elemento(int tipo);
+    void imprime_lista();
+
+    
+    
+    
+    
+
+    #include"TAD_listahete.c"
+#endif
